4730d6ed3bbc2801b09220a9249eaa500bccda98
Areef <shaikareef986@gmail.com>
2022-07-06 16:23:58 +0530

    First Commit....

---
+++ src/Test1.java
@@ -0,0 +1,18 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String hungry = "Yes";
+		
+		
+		if(hungry == "Yes") {
+			System.out.println("Eat somthing");
+			
+		}else {
+			System.out.println("Do Work");
+		}
+	
+
+	}
+
+}
